4fa31938cd634060dcb9ff826b5cbe7b5e04718e
laurahalsel <laura.halsel@gmail.com>
2026-06-23 11:30:12 -0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.PB_BOSSES_SUPABASE = {
-  url: "PASTE_SUPABASE_PROJECT_URL_HERE",
-  anonKey: "PASTE_SUPABASE_ANON_KEY_HERE",
+  url: "https://wqkntmljqtjcewjgsjpq.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indxa250bWxqcXRqY2V3amdzanBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyMzIzMzUsImV4cCI6MjA5NzgwODMzNX0.TE3GjC45j1726Zn-NyWNCkIlDrpE8OyqX0nZXyOG6pU",
 };
